53f70ca9b26c9dd2bf09e26196a02a4f279ae92f
Wayne Jeong <wayne.jeong.wj@gmail.com>
2020-03-30 23:23:35 +0900

    Create binaries with the /arch:AVX option.

--- matrix-multiplication-speed/Eigen/build.bat
+++ matrix-multiplication-speed/Eigen/build.bat
@@ -9,8 +9,20 @@ if not exist "%OUTPUT_FOLDER%" (mkdir "%OUTPUT_FOLDER%" 2>nul)
 
 cl.exe /EHsc /Fo%OUTPUT_FOLDER%\matrix_multiplication.obj /Fe%OUTPUT_FOLDER%\matrix_multiplication.exe matrix_multiplication.cpp
 
+REM With /arch:AVX compiler option
+set OUTPUT_FOLDER=_results\matrix_multiplication_avx
+if not exist "%OUTPUT_FOLDER%" (mkdir "%OUTPUT_FOLDER%" 2>nul)
+
+cl.exe /arch:AVX /EHsc /Fo%OUTPUT_FOLDER%\matrix_multiplication.obj /Fe%OUTPUT_FOLDER%\matrix_multiplication.exe matrix_multiplication.cpp
+
 REM With /openmp compiler option
 set OUTPUT_FOLDER=_results\matrix_multiplication_openmp
 if not exist "%OUTPUT_FOLDER%" (mkdir "%OUTPUT_FOLDER%" 2>nul)
 
 cl.exe /openmp /EHsc /Fo%OUTPUT_FOLDER%\matrix_multiplication.obj /Fe%OUTPUT_FOLDER%\matrix_multiplication.exe matrix_multiplication.cpp
+
+REM With /openmp and /arch:AVX compiler option
+set OUTPUT_FOLDER=_results\matrix_multiplication_openmp_avx
+if not exist "%OUTPUT_FOLDER%" (mkdir "%OUTPUT_FOLDER%" 2>nul)
+
+cl.exe /openmp /arch:AVX /EHsc /Fo%OUTPUT_FOLDER%\matrix_multiplication.obj /Fe%OUTPUT_FOLDER%\matrix_multiplication.exe matrix_multiplication.cpp
